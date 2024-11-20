b17635e9816839d335e58afeeb582bc0c862e3d1
kvtrice <katricemountford@gmail.com>
2024-11-20 17:43:03 +1100

    Updates layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 export const metadata: Metadata = {
-	title: "Kat Mountford | Software Engineer",
+	title: "Kat Mountford | Product Manager",
 	description:
-		"Kat Mountford is a Software Engineer with a flair for the front end and building great user experiences.",
+		"Kat Mountford is a Product Manager with experience across the full product life cycle, Full Stack Development & UX / UI Design. She's a multi-skilled professional passionate about using technology to create impactful solutions and deliver great user experiences.",
 };
 
 export default function RootLayout({
@@ -15,7 +15,10 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" data-theme="">
+		<html
+			lang="en"
+			data-theme=""
+		>
 			<body>
 				<Navbar />
 				{children}
